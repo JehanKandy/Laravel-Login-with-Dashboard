6eb4843056c820cdebfc2a1206e1cc27f5f232e3
JehanKandy <jehankandy@gmail.com>
2023-06-10 20:14:53 +0530

    Update

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,8 +19,5 @@
             </div>
         </div>
     </div>
-</div> --}}
-
-hi all I am  {{ Auth::user()->name }}
-
+</div>
 @endsection
