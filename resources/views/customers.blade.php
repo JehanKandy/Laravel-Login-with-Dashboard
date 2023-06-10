f2a82dbb379079d006f8512423b3aaf44d3eae6c
JehanKandy <jehankandy@gmail.com>
2023-06-10 22:15:53 +0530

    Update

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -113,8 +113,31 @@
     </div>
     <div class="col-lg-6">
         <div class="dash-card">
-    
-
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Address</th>
+                  </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                  </tr>
+                </tbody>
+              </table>
         </div>
     </div>
 </div>
